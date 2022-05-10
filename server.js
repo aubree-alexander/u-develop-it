094205e4e56b24d926c03d84b2270659d182c815
Aubree Alexander <aubree.alexander@gmail.com>
2022-05-09 20:55:09 -0500

    connect database to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT  = process.env.PORT || 3001;
@@ -7,7 +8,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
